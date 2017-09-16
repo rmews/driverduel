cce1878215f83e20a090aaa53a9dc95dc0e20c9a
Robert Mews <robert.mews@gmail.com>
2017-09-16 16:32:03 -0600

    Updated server config files again

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-import http.server
-import requests
-import os
-import threading
+// ./app.py
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from sqlalchemy import create_engine, func, desc
 from sqlalchemy.orm import sessionmaker
@@ -77,10 +74,6 @@ def showDriver(driver_name):
     return render_template('driver.html', driver=driver, results=results)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8000))    # Use PORT if it's there.
-    server_address = ('', port)
-
-# if __name__ == '__main__':
-#     app.run(debug=True, use_reloader=True)
-#     app.debug = True
-#     app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, use_reloader=True)
+    # app.debug = True
+    # app.run(host='0.0.0.0', port=5000)
